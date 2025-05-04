70bc63ad7f705114f692863bd7013fdeb849a9ea
kendallReyes <kendall14solr@gmail.com>
2025-05-04 16:52:45 -0600

    Function to load the information playlist added

--- JS/playListFunctions.js
+++ JS/playListFunctions.js
@@ -9,6 +9,12 @@ function getAdminIdFromUrl() {
     return params.get("adminId"); 
 }
 
+//Find the params playlistId in the url
+function getPlaylistIdFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("playlistId"); 
+}
+
 //Go back to the playListAdmin.html
 function goBackWithAdminId() {
     const adminId = getAdminIdFromUrl();
@@ -21,6 +27,10 @@ function goBackWithAdminId() {
 function getRestrictedUserIdFromUrl() {
     const params = new URLSearchParams(window.location.search);
     return params.get("profileId"); 
+}
+
+function redirectToPlaylistEdit(param) {
+    window.location.href = `../playListViews/playlistEdit.html?adminId=${getAdminIdFromUrl()}&playlistId=${param}`;
 }
 
 //Set a buttom to redirect to crate playlist page
@@ -73,9 +83,12 @@ function queryGetVideoCount(id) {
 function queryGetPlaylistByRestrictedUser(id) {
     const query =  `
         {
-            getPlayListByRestrictedUser(id: "${id}") {
+            getPlayListById(id: "${id}") {
                 _id
                 name
+                restrictedUsers {
+                    _id
+                }
             }
         }
     `;
@@ -103,6 +116,7 @@ async function fetchGraphQL(query) {
         }
 
         const data = result.data;
+        
         return data;
 
     } catch (error) {
@@ -184,13 +198,13 @@ async function getPlaylistsByAdmin() {
         for (const [index, playlist] of playlists.entries()) {
             const countVideos = await getVideoCountByPlayList(playlist._id);
             const row = document.createElement("tr");
-
+            
             row.innerHTML = `
                 <th scope="row">${index + 1}</th>
                 <td>${playlist.name}</td>
                 <td>${countVideos}</td>
                 <td>
-                    <button class="btn btn-primary btn-sm edit-btn" data-id="${playlist._id}">
+                    <button class="btn btn-primary btn-sm edit-btn" data-id="${playlist._id}" onclick="redirectToPlaylistEdit('${playlist._id}')">
                         <i class="fas fa-edit"></i> Edit
                     </button>
                     <button class="btn btn-danger btn-sm delete-btn" data-id="${playlist._id}" onclick="deletePlayList('${playlist._id}')">
@@ -206,42 +220,55 @@ async function getPlaylistsByAdmin() {
     }
 }
 
-// Delete playlist
-async function deletePlayList(playListId) {
-    const token = sessionStorage.getItem('jwtToken');
+//Get a playlist by ID to edit graphql
+async function getInfoPlaylistById() {
     try {
-        const response = await fetch(`http://127.0.0.1:3001/api/playList/${playListId}`, {
-            method: 'DELETE', 
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        });
+        const playlistData = await fetchGraphQL(queryGetPlaylistByRestrictedUser(getPlaylistIdFromUrl()));
+        const playlist = playlistData.getPlayListById;
+        let restrictedUserPlaylist = [];
+        restrictedUserPlaylist = playlist.restrictedUsers.map(user => user._id);
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            console.error("Error al eliminar la playlist:", errorData.error);
-            alert(`Error: ${errorData.error}`);  
-            return;
-        }
-
-        alert("PlayList deleted");
-
-        getPlaylistsByAdmin();
-
-    } catch (error) {
-        console.error("Error request delete:", error);
-        alert("Error request delete");
+        document.getElementById('videoName').value = playlist.name;
+        checkRestrictedUserByPlaylist(restrictedUserPlaylist);
+    }catch (error){
+        console.error("No possible load data in playlist edit páge:", error);
     }
 }
 
-//Take restricted users in check box WITH GraphQL
-async function getRestrictedUsers() {
+//Mark all checkboxes with restricted users from a playlist
+async function checkRestrictedUserByPlaylist(selectedIds = []) {
     try {
         const restrictedUsersData =  await fetchGraphQL(queryRestrictedUserByCheckBox());
         const restrictedUsers = restrictedUsersData.getRestrictedUserByAdmin;
-        console.log("Mensaje desde el cliente, usuarios restringidos: ", restrictedUsers)
 
+        const container = document.getElementById("restrictedUsersContainer");
+        container.innerHTML = ""; 
+
+        restrictedUsers.forEach(user => {
+            const isChecked = selectedIds.includes(user._id) ? "checked" : "";
+
+            const userDiv = document.createElement("div");
+            userDiv.classList.add("form-check");
+
+            userDiv.innerHTML = `
+                <input class="form-check-input" type="checkbox" value="${user._id}" id="user-${user._id}" ${isChecked}>
+                <label class="form-check-label" for="user-${user._id}">${user.name}</label>
+            `;
+
+            container.appendChild(userDiv);
+        });
+
+    } catch (error) {
+        console.error("Error al obtener los usuarios restringidos:", error);
+    }
+}
+
+
+//Take restricted users in check box WITH GraphQL
+async function getRestrictedUsersCheckBox() {
+    try {
+        const restrictedUsersData =  await fetchGraphQL(queryRestrictedUserByCheckBox());
+        const restrictedUsers = restrictedUsersData.getRestrictedUserByAdmin;
         
         if (restrictedUsers.length === 0) {
             console.log("The admin don´t has restricted users");
@@ -284,6 +311,35 @@ function getSelectedUsers() {
 
     console.log("Usuarios seleccionados:", selectedUsers);
     return selectedUsers;
+}
+
+// Delete playlist
+async function deletePlayList(playListId) {
+    const token = sessionStorage.getItem('jwtToken');
+    try {
+        const response = await fetch(`http://127.0.0.1:3001/api/playList/${playListId}`, {
+            method: 'DELETE', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.error("Error al eliminar la playlist:", errorData.error);
+            alert(`Error: ${errorData.error}`);  
+            return;
+        }
+
+        alert("PlayList deleted");
+
+        getPlaylistsByAdmin();
+
+    } catch (error) {
+        console.error("Error request delete:", error);
+        alert("Error request delete");
+    }
 }
 
 //Create new playlist
@@ -331,11 +387,13 @@ async function createPlaylist() {
 }
 
 
-
 function isCreatePlaylistPage() {
     return window.location.pathname.includes("playListCreateAdmin.html");
 }
 
+function isEditPlaylistPage() {
+    return window.location.pathname.includes("playlistEdit.html");
+}
 
 function isPlayListAdminPage() {
     return window.location.pathname.includes("playListAdmin.html");
@@ -353,8 +411,13 @@ document.addEventListener("DOMContentLoaded", function() {
         createPlaylistButton();
     }
 
+    if(isEditPlaylistPage()) {
+        getInfoPlaylistById()
+        getRestrictedUsersCheckBox();
+    }
+
     if (isCreatePlaylistPage()) {
-        getRestrictedUsers(); 
+        getRestrictedUsersCheckBox(); 
     }
 
     if (isPlaylistRestrictedUSerPage()) {
